589eef9f72adf60a599f3e28053b4f75f3358f43
Luskalves <lusca.alves32@gmail.com>
2022-08-21 10:33:15 -0300

    metodo role criado no LoginService

--- app/backend/src/services/loginServices.ts
+++ app/backend/src/services/loginServices.ts
@@ -6,6 +6,7 @@ import BadRequest from '../errors/badRequest';
 import NotAuthorized from '../errors/notAuthorized';
 import Users from '../database/models/Users';
 import ILogin from '../interfaces/ILogin';
+import { TypeUser } from '../interfaces/types/TypeUser';
 
 const secret = String(process.env.JWT_SECRET);
 
@@ -15,6 +16,20 @@ export const schema = Joi.object({
 });
 
 export default class LoginService {
+  static async role(token: string) {
+    const emailDecoded = jwt.decode(token);
+
+    const user: TypeUser = await Users.findOne({
+      where: {
+        email: emailDecoded,
+      },
+    });
+
+    if (user?.role) {
+      return user.role;
+    }
+  }
+
   static async find(body: ILogin) {
     const { error } = schema.validate(body);
     if (error) throw new BadRequest('All fields must be filled');
