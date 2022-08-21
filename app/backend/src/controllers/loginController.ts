9cbc2c21945d520374d43683982b3762cd243b76
Luskalves <lusca.alves32@gmail.com>
2022-08-21 10:32:21 -0300

    metodo get criado no LoginController

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -2,6 +2,16 @@ import { NextFunction, Request, Response } from 'express';
 import LoginService from '../services/loginServices';
 
 export default class LoginController {
+  static async get(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = String(req.headers.authorization);
+      const role = await LoginService.role(token);
+      res.status(200).json({ role });
+    } catch (e) {
+      next(e);
+    }
+  }
+
   static async post(req: Request, res: Response, next: NextFunction) {
     try {
       const token = await LoginService.find(req.body);
