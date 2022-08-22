036106fb4ffb5f92f343f33ba274c2d5663b0ccc
Luskalves <lusca.alves32@gmail.com>
2022-08-22 14:44:05 -0300

    model do Teams criado

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,26 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id!: number;
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
