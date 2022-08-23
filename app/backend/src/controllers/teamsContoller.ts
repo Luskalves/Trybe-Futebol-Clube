1b284a622c1e8e0f6eb48cf3f2d2612405418128
Luskalves <lusca.alves32@gmail.com>
2022-08-23 16:56:12 -0300

    teamsController refatorado

--- app/backend/src/controllers/teamsContoller.ts
+++ app/backend/src/controllers/teamsContoller.ts
@@ -1,24 +1,16 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import TeamsService from '../services/teamsService';
 
 export default class TeamsController {
-  static async get(_req: Request, res: Response, next: NextFunction) {
-    try {
-      const teams = await TeamsService.findAll();
+  static async get(_req: Request, res: Response) {
+    const teams = await TeamsService.findAll();
 
-      res.status(200).json(teams);
-    } catch (e) {
-      next(e);
-    }
+    res.status(200).json(teams);
   }
 
-  static async findOne(req: Request, res: Response, next: NextFunction) {
-    try {
-      const id = Number(req.params.id);
-      const team = await TeamsService.findOne(id);
-      res.status(200).json(team);
-    } catch (e) {
-      next(e);
-    }
+  static async findOne(req: Request, res: Response) {
+    const id = Number(req.params.id);
+    const team = await TeamsService.findOne(id);
+    res.status(200).json(team);
   }
 }
