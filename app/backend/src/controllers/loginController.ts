ea17f82a122ff72564eac7b946e5369aa960a482
Luskalves <lusca.alves32@gmail.com>
2022-08-23 15:43:16 -0300

    loginController refatorado retirando o try/catch

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -1,23 +1,15 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import LoginService from '../services/loginServices';
 
 export default class LoginController {
-  static async get(req: Request, res: Response, next: NextFunction) {
-    try {
-      const token = String(req.headers.authorization);
-      const role = await LoginService.role(token);
-      res.status(200).json({ role });
-    } catch (e) {
-      next(e);
-    }
+  static async get(req: Request, res: Response) {
+    const token = String(req.headers.authorization);
+    const role = await LoginService.role(token);
+    res.status(200).json({ role });
   }
 
-  static async post(req: Request, res: Response, next: NextFunction) {
-    try {
-      const token = await LoginService.find(req.body);
-      res.status(200).json({ token });
-    } catch (e) {
-      next(e);
-    }
+  static async post(req: Request, res: Response) {
+    const token = await LoginService.find(req.body);
+    res.status(200).json({ token });
   }
 }
