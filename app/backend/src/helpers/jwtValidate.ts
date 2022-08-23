8e67edd0b3327b86862d56badbce0c0988d78c6b
Luskalves <lusca.alves32@gmail.com>
2022-08-23 18:22:59 -0300

    lógica do jwtValidate refatorada

--- app/backend/src/helpers/jwtValidate.ts
+++ app/backend/src/helpers/jwtValidate.ts
@@ -1,15 +1,18 @@
 import * as jwt from 'jsonwebtoken';
-import NotAuthorized from '../errors/notAuthorized';
 
 const secret = String(process.env.JWT_SECRET);
 
 const jwtValidade = {
   async validate(token: string) {
     try {
-      const email = jwt.verify(token, secret);
-      return email;
+      jwt.verify(token, secret, (err) => {
+        if (err) {
+          throw new Error();
+        }
+      });
+      return true;
     } catch (e) {
-      throw new NotAuthorized('Token must be a valid token');
+      return undefined;
     }
   },
 };
