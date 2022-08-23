903bf8b9be4a31f6b850779e69298b36f4bf10f4
Luskalves <lusca.alves32@gmail.com>
2022-08-23 15:19:49 -0300

    metodo get do controller configurado

--- app/backend/src/controllers/matchesController.ts
+++ app/backend/src/controllers/matchesController.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
+import MatchesService from '../services/matchesService';
 
 export default class MacthesController {
   static async get(_req: Request, res: Response, next: NextFunction) {
     try {
-      const matches = ['1'];
+      const matches = await MatchesService.findAll();
       res.status(200).json(matches);
     } catch (e) {
       next(e);
