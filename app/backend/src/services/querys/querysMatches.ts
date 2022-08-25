6abf12dbd450043f70a8529093ebf31c702f8dad
Luskalves <lusca.alves32@gmail.com>
2022-08-25 19:10:08 -0300

    awayQuery modificada no querysMatches

--- app/backend/src/services/querys/querysMatches.ts
+++ app/backend/src/services/querys/querysMatches.ts
@@ -42,6 +42,48 @@ const homeQuery = `
   order by totalPoints desc, goalsBalance desc, goalsFavor desc, goalsOwn;
 `;
 
-const awayQuery = 'oioi';
+const awayQuery = `
+  select 
+    t.team_name as 'name',
+      sum(m.away_team_goals > m.home_team_goals) * 3 + 
+      (sum(m.home_team_goals = m.away_team_goals)) as 'totalPoints',
+      count(m.away_team) as 'totalGames',
+    sum(
+      case 
+        when m.away_team_goals > m.home_team_goals then 1
+        else 0
+          end
+      ) as 'totalVictories',
+      sum(m.home_team_goals = m.away_team_goals) as 'totalDraws',
+      sum(m.home_team_goals > m.away_team_goals) as 'totalLosses',
+      sum(m.away_team_goals) as 'goalsFavor',
+      sum(m.home_team_goals) as 'goalsOwn',
+      sum(m.away_team_goals) - sum(m.home_team_goals) as 'goalsBalance',
+    round((sum(
+      case 
+        when m.away_team_goals > m.home_team_goals then 3
+        else 0
+          end
+      ) + sum(
+      case 
+        when  m.home_team_goals = m.away_team_goals then 1
+              else 0
+      end
+      )
+      ) / (
+      sum(
+        case
+          when m.away_team = t.id then 1
+                  else 0
+        end
+          ) * 3
+      ) * 100, 2) as 'efficiency'   
+  from teams as t
+  inner join matches as m
+  on t.id = m.away_team
+  where m.in_progress = 0
+  group by t.team_name
+  order by totalPoints desc, goalsBalance desc, goalsFavor desc, goalsOwn;
+`;
 
 export { homeQuery, awayQuery };
