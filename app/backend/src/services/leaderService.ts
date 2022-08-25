1934664c02caef9f523a7f1a51804321f16a1d17
Luskalves <lusca.alves32@gmail.com>
2022-08-25 19:10:35 -0300

    metodo getAway criado no leaderService

--- app/backend/src/services/leaderService.ts
+++ app/backend/src/services/leaderService.ts
@@ -1,10 +1,16 @@
-import { homeQuery } from './querys/querysMatches';
+import { homeQuery, awayQuery } from './querys/querysMatches';
 import model from '../database/models';
 
 export default class LeaderService {
   constructor(public leaderModel = model) {}
   public async getHome() {
     const [querys] = await this.leaderModel.query(homeQuery);
+
+    return querys;
+  }
+
+  public async getAway() {
+    const [querys] = await this.leaderModel.query(awayQuery);
 
     return querys;
   }
