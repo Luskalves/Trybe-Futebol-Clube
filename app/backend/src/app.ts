dd49639e8d3b15c25cc4ac9e4c981f9b5a14357d
Luskalves <lusca.alves32@gmail.com>
2022-08-25 17:49:37 -0300

    rota de leaderboards adcionada no app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -3,6 +3,7 @@ import errorMiddleware from './middlewares/errorMiddleware';
 import loginRoute from './routes/loginRoute';
 import teamsRoute from './routes/teamsRoute';
 import matchesRoute from './routes/matchesRoute';
+import leaderRoute from './routes/leaderRoute';
 
 class App {
   public app: express.Express;
@@ -30,6 +31,7 @@ class App {
     this.app.use('/login', loginRoute);
     this.app.use('/teams', teamsRoute);
     this.app.use('/matches', matchesRoute);
+    this.app.use('/leaderboard', leaderRoute);
     this.app.use(errorMiddleware);
   }
 
